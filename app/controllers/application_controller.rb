5974fa5d16a9bcb64a9995b8cc887e4a0c7c07f5
Megumu-Suzuki <m.suzuki.te@gmail.com>
2021-09-06 10:04:15 +0000

    ユーザーの詳細ページ、その遷移先ページの作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,19 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  #会員登録後の遷移先
+  def after_sign_up_path_for(resource)
+    user_path(params[:id])
+  end
+  #ログアウト後の遷移先
+  def after_sign_out_path_for(resource)
+    root_path
+  end
 
   protected
     #会員登録時のストロングパラメーター
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana])
     end
-    #会員登録後の遷移先
-    def after_sign_up_path_for(resource)
-      user_path(params[:id])
-    end
-
+    
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  #プロフィール画像
+  has_one_attached :profile_image
 
   def full_name
     "#{last_name} #{first_name}"
