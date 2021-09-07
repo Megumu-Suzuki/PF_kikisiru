44bc522e11c6a754b924fb41c36775fedda56be8
Megumu-Suzuki <m.suzuki.te@gmail.com>
2021-09-07 03:26:30 +0000

    商品に関するページを作成

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -11,23 +11,33 @@ class Public::ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
+    render :new and return if params[:back]
     @product.user_id = current_user.id
     if @product.save
-      redirect_to product_path(@product.id), notice: "商品を追加しました"
+      redirect_to product_path(@product.id), notice: "機器を投稿しました"
     else
-      flash.now[:alert] = "商品の追加に失敗しました"
+      flash.now[:alert] = "機器の投稿に失敗しました"
       redirect_to new_product_path
       @product = Product.new(product_params)
     end
   end
 
   def show
+    @product = Product.find(params[:id])
   end
 
   def edit
+    @product = Product.find(params[:id])
   end
 
   def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to product_path(@product.id), notice: "変更を保存しました"
+    else
+      flash.now[:alert] = "変更の保存に失敗しました"
+      render :edit
+    end
   end
 
   private
--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,6 @@ class Product < ApplicationRecord
   accepts_nested_attributes_for :product_images, allow_destroy: true
 
   enum manufacture: { "A社": 0, "B社": 1, "C社": 2, "D社": 3, "E社": 4, "その他": 5}
-  enum phase: { "なし": 0, "単相100V": 1, "単相200V": 2, "3相200V": 3}
+  enum phase: { "1φ100V": 0, "1φ200V": 1, "3φ200V": 2}
 
 end
