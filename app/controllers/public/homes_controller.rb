119de5af7e8d431c39fd36fb7bfe6da675a19a19
Megumu-Suzuki <m.suzuki.te@gmail.com>
2021-10-04 06:13:47 +0000

    ランキング機能修正中

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -4,13 +4,8 @@ class Public::HomesController < ApplicationController
     @genres = Genre.all.includes(:products)
     @tags = Tag.limit(20).order("id DESC")
     @genre_rankings = @genres.each do |genre|
-      Product.left_joins(:reviews).where(genre_id: genre.id).includes([:reviews]).distinct.sort_by do |product|
-        ranking = product.reviews
-        if ranking.present?
-          ranking.map(&:evaluation).sum / ranking.size
-        else
-          0
-        end
+      genre.products.sort_by do |product|
+        product.review_average
       end.reverse
     end
   end
